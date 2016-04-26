7a8999aa8eb11ad92ad4699adf0f1b31e1126c01
saung <sithu_aung@intuit.com>
2016-04-26 14:51:56 -0700

    react state change

--- App.jsx
+++ App.jsx
@@ -5,62 +5,29 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = { data: 0 }
-		this.setNewNumber = this.setNewNumber.bind(this);
+		this.state = { data: 'Initial Data' }
+		this.updateState = this.updateState.bind(this);
 	}
 
-	setNewNumber() {
-		this.setState({ data: this.state.data + 1});
+	updateState(e) {
+		this.setState({ data: e.target.value});
 	}
 	
 	render() {
 		return (
 			<div>
-				<button onClick= {this.setNewNumber}>Increment</button>
-				<Content myNumber = {this.state.data}></Content>
+				<Content myDataProp= {this.state.data} updateStateProp={this.updateState}></Content>
 			</div>
 		);
 	}
 }
 
 class Content extends React.Component {
-	// this will execute before render
-	componentWillMount() {
-		console.log("Component will mount")
-	}
-
-	render() {
-		console.log("render")
-	}
-
-	componentDidMount() {
-		console.log("Component did mount")
-	}
-
-	componentWillReceiveProps(newProps) {
-		console.log("Will receive prorps")
-	}
-
-	shouldComponentUpdate(newProps, newState) {
-		return true;
-	}
-
-	componentWillUpdate(nextProps, nextState) {
-		console.log("Will update...");
-	}
-
-	componentDidUpdate(prevProps, prevState) {
-		console.log("Did Update...");
-	}
-	
-	componentWillUnmount() {
-		console.log("Will Unmount...");
-	}
-
 	render() {
 		return (
 			<div>
-				<h3>{this.props.myNumber}</h3>
+				<input type="text" value={this.props.myDataProp} onChange={this.props.updateStateProp}/>
+				<h3>{this.props.myDataProp}</h3>
 			</div>
 		);
 	}
--- main.js
+++ main.js
@@ -4,8 +4,3 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'))
-
-setTimeout(() => {
-	ReactDOM.unmountComponentAtNode(document.getElementById('app'));},
-	10000
-)
