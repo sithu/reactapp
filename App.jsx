b389991913aebe5bcfb8fa54e2b2664f7d01881d
saung <sithu_aung@intuit.com>
2016-04-26 11:54:59 -0700

    different ways of passing state

--- App.jsx
+++ App.jsx
@@ -4,21 +4,40 @@ class App extends React.Component {
 
 	constructor() {
 		super();
+		this.state = {
+			"header": "Default Header",
+			"content": "Default Content"
+		}
 	}
 
 	render() {
 		return (
 			<div>
-				<h1>{this.props.headerProp}</h1>
-				<h2>{this.props.contentProp}</h2>
+				<Header headerProp={this.state.header}/>
+				<Content contentProp={this.state.content}/>
 			</div>
 		);
 	}
 }
 
-App.defaultProps = {
-		"headerProp": "Default Header",
-		"contentProp": "Default Content"
+class Header extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{this.props.headerProp}</h1>
+			</div>
+		);
+	}
+}
+
+class Content extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{this.props.contentProp}</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
