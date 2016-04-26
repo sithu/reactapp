1a4d2798b9d6d03d373e7c3e78ef54d55bc2e855
saung <sithu_aung@intuit.com>
2016-04-26 14:10:06 -0700

    add react dom

--- App.jsx
+++ App.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+
+	constructor() {
+		super();
+		this.state = { data: [] }
+		this.setStateHandler = this.myStateHandler.bind(this)
+		this.forceUpdateHandler = this.forceUpdateHandler.bind(this)
+		this.getDomNodeHandler = this.findDomNodeHandler.bind(this)
+	}
+
+	findDomNodeHandler() {
+		var myDiv = document.getElementById('myDiv')
+		React.findDOMNode("myDiv").style.color = 'green'
+	}
+
+	forceUpdateHandler() {
+		this.forceUpdate()
+		this.setState({ data: []})
+	}
+
+	myStateHandler() {
+		var item = "setState..."
+		var myArray = this.state.data
+		myArray.push(item)
+		this.setState({data: myArray})
+	}
 
 	render() {
 		return (
 			<div>
-				<h3>Array: {this.props.propArray}</h3>
-				<h3>Bool: {this.props.propBool} ? "True" : "False"</h3>
-				<h3>Func: {this.props.propFunc (3)}</h3>
-				<h3>Object: {this.props.ObjectName}</h3>
+				<button onClick={this.forceUpdateHandler}>FORCE</button>
+				<button onClick={this.setStateHandler}>SET STATE</button>
+				<h4>State Array: {this.state.data}</h4>
+				<h4>Random Num: {Math.random()}</h4>
+				<button onClick={this.findDomNodeHandler}>Find Me</button>
+				<div id="myDiv">NODE</div>
 			</div>
 		);
 	}
